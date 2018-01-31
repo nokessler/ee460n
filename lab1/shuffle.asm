fe6bb1ed86841cd39489d16eb26c5ed523a10fcc
nokessler <noah@kesslerhome.com>
2018-01-30 18:22:07 -0600

    removed unnecessary lines

--- lab1/shuffle.asm
+++ lab1/shuffle.asm
@@ -1,36 +1,35 @@
-   .ORIG x3000
+   .ORIG x3000             ;0x3000
    ;INITIALIZATION, LOAD x4000 INTO R0 AND CONTROL MASK INTO R1
-   LEA R1, ADR
-   LDW R0, R1, #0
-   LDB R1, R0, #4
+   LEA R1, ADR             ;0xE219
+   LDW R0, R1, #0          ;0x6040
+   LDB R1, R0, #4          ;0x2204
    ;STORE DATA IN LOOP
    ;R7 IS COUNTER
    ;FIRST RUN STORES DATA IN x4005
-   AND R7, R7, #0
+   AND R7, R7, #0          ;0x5FE0
    ;DETERMINE WHICH DATA TO LOAD
-LOOP  ADD R2, R1, #0
-   ADD R6, R7, R7
-   BRZ MASK
-   ADD R5, R7, #-1
-   BRZ ONE
-   ADD R5, R7, #-2
-   BRZ TWO
-   RSHFA R2, R2, #6
-   BRNZP MASK
-TWO   RSHFA R2, R2, #4
-   BRNZP MASK
-ONE   RSHFA R2, R2, #2
-   BRNZP MASK
-MASK  AND R2, R2, x0003
+LOOP  ADD R2, R1, #0       ;0x1460
+   ADD R6, R7, R7          ;0x1DC7
+   BRZ MASK                ;0x0409
+   ADD R5, R7, #-1         ;0x1BFF
+   BRZ ONE                 ;0x0406
+   ADD R5, R7, #-2         ;0x1BFE
+   BRZ TWO                 ;0x0402
+   RSHFA R2, R2, #6        ;0xD4B6
+   BRNZP MASK              ;0x0E03
+TWO   RSHFA R2, R2, #4     ;0xD4B4
+   BRNZP MASK              ;0x0E01
+ONE   RSHFA R2, R2, #2     ;0xD4B2
+MASK  AND R2, R2, #3       ;0x54A3
    ;LOAD AND STORE DATA
-   ADD R2, R0, R2
-   LDB R3, R2, #0
-   ADD R6, R0, #5
-   ADD R6, R6, R7
-   STB R3, R6, #0
-   ADD R7, R7, #1
-   ADD R6, R7, #-4
-   BRN LOOP
-   HALT
-ADR   .FILL x4000
+   ADD R2, R0, R2          ;0x1402
+   LDB R3, R2, #0          ;0x2680
+   ADD R6, R0, #5          ;0x1C25
+   ADD R6, R6, R7          ;0x1D87
+   STB R3, R6, #0          ;0x3780
+   ADD R7, R7, #1          ;0x1FE1
+   ADD R6, R7, #-4         ;0x1DFC
+   BRN LOOP                ;0x09EB
+   HALT                    ;0xF025
+ADR   .FILL x4000          ;0x4000
    .END
